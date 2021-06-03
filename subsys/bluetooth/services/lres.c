b2deb10139ccde7bf163b48bc0d34546f51ade90
lorenz11 <lorenz_meierhofer@gmx.de>
2021-06-03 22:04:20 +0200

    y

--- subsys/bluetooth/services/lres.c
+++ subsys/bluetooth/services/lres.c
@@ -53,6 +53,7 @@ static ssize_t change_config_cb(struct bt_conn *conn, const struct bt_gatt_attr 
 	struct lora_modem_config config;
 	int ret;
 
+	/*
 	uint8_t *pu = (uint8_t *) buf;
 	int frequencies[8] =  {868000000, 868300000, 868500000, 867100000, 867300000, 867500000, 867700000, 869500000};
 	
@@ -77,6 +78,15 @@ static ssize_t change_config_cb(struct bt_conn *conn, const struct bt_gatt_attr 
 	config.tx_power = *pu + 5;
 
 	config.tx = false;
+	*/
+
+	config.frequency = 868000000;
+	config.bandwidth = BW_125_KHZ;
+	config.datarate = SF_10;
+	config.preamble_len = 8;
+	config.coding_rate = CR_4_5;
+	config.tx_power = 4;
+	config.tx = false;
 
  	lora_dev = device_get_binding(DEFAULT_RADIO);
 	if (!lora_dev) {
@@ -90,7 +100,7 @@ static ssize_t change_config_cb(struct bt_conn *conn, const struct bt_gatt_attr 
 
 	int8_t bt_data[1] = {-2};
 	bt_lres_notify(bt_data, 2);
-	printk("[NOTIFICATION] data %d length %u\n", *pu, len);
+	//printk("[NOTIFICATION] data %d length %u\n", *pu, len);
 	return 0;
 }
 
