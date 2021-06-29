c0459e170e779424c0431974162215fd2fa31a37
lorenz11 <lorenz_meierhofer@gmx.de>
2021-06-29 11:56:33 +0200

    randomize payload

--- subsys/bluetooth/services/lses.c
+++ subsys/bluetooth/services/lses.c
@@ -282,13 +282,15 @@ K_THREAD_STACK_DEFINE(stack_area1, STACK_SIZE);
 struct k_thread thread_data1;
 k_tid_t thread1_tid;
 
-uint8_t random_d [200] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,
+/*uint8_t random_d [200] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,
 					37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,
 					73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,
 					107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,
 					134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,
 					161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,
-					188,189,190,191,192,193,194,195,196,197,198,199};
+					188,189,190,191,192,193,194,195,196,197,198,199};*/
+
+uint8_t random_d [200];					
 
 // experiment send thread code
 void exec_experiment(void *a, void *b, void *c) {
@@ -433,11 +435,23 @@ void exec_experiment(void *a, void *b, void *c) {
 }
 
 
-
+uint16_t payload_index = 0;
 // prepare sender for experiment and start sending thread or cancel prepared sender
 static ssize_t prepare_sender_cb(struct bt_conn *conn, const struct bt_gatt_attr *attr,
 			 const void *buf, uint16_t len, uint16_t offset, uint8_t sth)
 {
+	char *pc = (char *) buf;
+	if(len == 20) {
+		random_d[payload_index] = *pc;
+		payload_index++;
+		pc++;
+		return;
+	}
+	payload_index = 0;
+	for(int16_t i = 0; i < 200; i++) {
+		printk(", %d", random_d[i]);
+	}
+
 	const struct device *lora_dev;
 	lora_dev = device_get_binding(DEFAULT_RADIO);
 
@@ -450,8 +464,6 @@ static ssize_t prepare_sender_cb(struct bt_conn *conn, const struct bt_gatt_attr
 		}
 		return 0;
 	}
-
-	char *pc = (char *) buf;
 
 	// configure and prepare experiment mode
 	change_config(pc, false);
